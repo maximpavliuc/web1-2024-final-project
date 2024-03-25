53d17775580ab66d0b527eeba819b0ee7199ba78
Maxim Pavliuc <maksik.pavlyuk@mail.ru>
2024-03-25 23:29:26 +0200

    search

--- specsavy/main/src/App.jsx
+++ specsavy/main/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./common/header/Header"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+      <Router>
+        <Header/>
+        <Switch>
+          
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App;
